66cc0f2a0d9a3db0bb0f28e8d4136e3ff10d5b72
sambee <haigenwong@126.com>
2013-11-20 17:58:56 +0800

    [update project.]

--- README.md
+++ README.md
@@ -10,14 +10,25 @@ example:
 - The string 'echo plase enter your command here.' in any2swf.bat replace as you want the command line
 -- so you try to your command:
 -- 
+
 -- any2swf yourfile.png output.swf
 -- any2swf c:\\01.docx d:\\02.swf
-   
+
+jpeg files to pdf:
+
+-- any2swf d:/imgs/*.jpg 1.pdf
  
--- @link
+--
 you can use imagemagick to convert tiff to pdf
+I install "ImageMagick-6.8.7-6-Q16-x64-static.exe" after, I test and pass in my system.
+
+-- @see
 http://www.imagemagick.org/script/binary-releases.php#windows
 http://www.ibm.com/developerworks/library/l-graf2/#N10284
+http://www.imagemagick.org/Usage/layers/
 
 you can use swftools to convert pdf to swf
 http://www.swftools.org/download.html
+
+@author£¨×÷Õß£©
+HTTP: <http://user.qzone.qq.com/173482035/main>
--- any2swf.bat
+++ any2swf.bat
@@ -1,16 +1,43 @@
 @echo off
-set ext=%~x1
-set SWFTOOLS_PATH=.\swftools
-if %ext% ==.txt (goto txt)
-if %ext% ==.doc (goto doc)
-if %ext% ==.docx (goto docx)
-if %ext% ==.jpg (goto jpg)
-if %ext% ==.jpeg (goto jpeg)
-if %ext% ==.png (goto png)
 
+set SWFTOOLS_PATH=.\swftools
+set IMAGEMAGICK=.\ImageMagick
+set CURR_DIR=%~dp0
+
+set SRC_DIR=%~dp1
+set SRC_NAME=%~n1
+set SRC_EXT=%~x1
+set SRC_FULL_NAME=%~nx1
+
+set DESC_DIR=%~dp2
+set DESC_NAME=%~n2
+set DESC_EXT=%~x2
+set DESC_FULL_NAME=%~nx2
+set PARAM_1=%1
+set PARAM_2=%2
+cls
+
+rem echo CURR_DIR : %CURR_DIR% 
+
+rem echo SRC_NAME : %SRC_NAME% 
+rem echo SRC_DIR : %SRC_DIR% 
+rem echo SRC_EXT : %SRC_EXT% 
+rem echo SRC_FULL_NAME : %SRC_FULL_NAME% 
+
+rem echo DESC_DIR : %DESC_DIR% 
+rem echo DESC_NAME : %DESC_NAME% 
+rem echo DESC_EXT : %DESC_EXT% 
+rem echo DESC_FULL_NAME : %DESC_FULL_NAME% 
+
+if %SRC_EXT% ==.txt (goto txt)
+if %SRC_EXT% ==.doc (goto doc)
+if %SRC_EXT% ==.docx (goto docx)
+if %SRC_EXT% ==.jpg (goto jpg)
+if %SRC_EXT% ==.jpeg (goto jpeg)
+if %SRC_EXT% ==.png (goto png)
 
 :unknow
-  echo "unkonw %ext%"
+  echo "unkonw %SRC_EXT%"
   goto quit
 
 :txt
@@ -24,7 +51,7 @@ if %ext% ==.png (goto png)
   echo "%1 to %~n1.pdf"
   cscript docx2pdf.js %1 %~dp1%~n1.pdf
   cd %SWFTOOLS_PATH%
-  pdf2swf.exe -p 1-20 -B rfxview.swf -o %2  %~dp1%~n1.pdf
+  pdf2swf.exe -p 1-20 -B rfxview.swf -o %2  %~dp1%~n1.swf
   cd ..
   goto quit
 
@@ -34,8 +61,12 @@ if %ext% ==.png (goto png)
   goto quit
 
 :jpg
-  echo JPEG File
-  echo plase enter your command here.
+  echo Convert JPEG  File
+
+  %IMAGEMAGICK%\convert.exe %PARAM_1% %DESC_DIR%\%SRC_NAME%.pdf
+  cd %SWFTOOLS_PATH%
+  pdf2swf.exe -p 1-20 -B rfxview.swf -o %DESC_DIR%\%DESC_FULL_NAME%  %DESC_DIR%\%SRC_NAME%.pdf
+  del /f %DESC_DIR%\%SRC_NAME%.pdf
   goto quit
 
 :tif
@@ -49,3 +80,4 @@ if %ext% ==.png (goto png)
   goto quit
 
 :quit
+cd %CURR_DIR%
